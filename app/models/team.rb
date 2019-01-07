c040b58593e235e7225f2a118081e5f6cdf3b544
Sharmaine Requerme <srqrme@gmail.com>
2019-01-07 23:28:25 +0000

    Done.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,16 +1,8 @@
 class Team
   attr_accessor :name, :motto
 
-  TEAMS = []
-
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
-    TEAMS << self
   end
-
-  def self.all
-    TEAMS
-  end
-
 end
